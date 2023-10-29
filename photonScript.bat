448597bb99210f3ee94d86a0bdab6ca192348de5
Alex Rabochii <cdx9933@gmail.com>
2023-10-29 00:02:45 -0400

    batch script

---
+++ photonScript.bat
@@ -0,0 +1,29 @@
+@Echo off
+setlocal EnableExtensions EnableDelayedExpansion
+SET /p option="Would you like to pull or push data to the Pi (pull/push): "
+SET dirStored=""
+
+IF !option!==pull (
+    SET /p "dirStored=Enter path to store the config folder in: "
+    ECHO !dirStored!
+
+    IF !dirStored!=="" (
+        ECHO No input given for the directory
+        EXIT /b 2
+    )
+    START CMD /k "scp -r root@10.6.95.12:/opt/photonvision/photonvision_config !dirStored!" 
+    EXIT /b 0
+)
+
+IF !option!==push (
+    SET /p "dirStored=Enter path to your config folder: "
+    ECHO !dirStored!
+
+    IF !dirStored!=="" (
+        ECHO No input given for the directory
+        EXIT /b 2
+    )
+
+    START CMD /k "scp -r !dirStored! root@10.6.95.12:/opt/photonvision/" 
+    EXIT /b 0
+)
